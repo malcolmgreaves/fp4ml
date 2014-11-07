3d561311ee410d49df24c423ae9a5374fff88016
Malcolm Greaves <malcolm@alpinenow.com>
2014-11-07 14:08:12 -0800

    wip -- 20 newsgroups document ranking experiment, word-count vs TFIDF based vectorization

--- src/test/scala/mlbigbook/wordcount/TwentyNewsgroupsExperimentTest.scala
+++ src/test/scala/mlbigbook/wordcount/TwentyNewsgroupsExperimentTest.scala
@@ -1,15 +1,48 @@
 package mlbigbook.wordcount
 
-import org.scalatest.{ BeforeAndAfterAll, Suite, FunSuite }
+import org.scalatest.FunSuite
 
-import org.apache.spark.SparkContext
+class TwentyNewsgroupsExperimentTest extends FunSuite with LocalSparkContext {
 
-import org.apache.log4j.{ Level, Logger }
+  import TwentyNewsgroupsExperimentTest._
 
-object ParseData {
+  lazy val trainDocs20Newsgroups: Data.Corpus = ??? // sc.textFile train 20 newsgroups data...
+  lazy val testDocs20Newsgroups:Data.Corpus = ??? // sc.textFile test 20 newsgroups data...
 
-  // def parse(f:File):SeqData[Data.Document] = {
+  test("[experiment] 20 newsgroups document ranking, wordcount vs. TFIDF based vectorization") {
+    doExperimentPrintResults(docLimit)(trainDocs20Newsgroups, testDocs20Newsgroups)
+  }
 
-  // }
+  def doExperimentPrintResults(docLimit:Int)(train:DistData[Data.Document], test:DistData[Data.Document]):Unit = {
 
+    val rankWC = Rank(
+      Vector.cosineSimilarity,
+      docLimit,
+      VectorTest.wordcountVectorizer,
+      train)
+
+    val rankTFIDF = Rank(
+      Vector.cosineSimilarity,
+      docLimit,
+      VectorTest.tfidfVectorizer,
+      train)
+
+    test.map(doc => {
+
+      val topdocsWC = rankWC(doc)
+      val topdocsTFIDF = rankTFIDF(doc)
+
+      println(s"Query document: $doc")
+      topdocsWC.toSeq.zip(topdocsTFIDF.toSeq).foreach({
+        case (rdocWC, rdocTFIDF) => println(s"Ranked documents, WC: $rdocWC  TFIDF: $rdocTFIDF")
+      })
+      println("")
+    })
+  }
+
+}
+
+object TwentyNewsgroupsExperimentTest {
+
+  val docLimit = 10
 }
