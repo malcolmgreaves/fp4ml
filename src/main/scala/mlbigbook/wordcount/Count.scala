a9beb5006da0774a9d4293598817510a9cf7ce17
Malcolm Greaves <malcolm@alpinenow.com>
2014-11-07 17:50:59 -0800

    removed WordcountDriver

--- src/main/scala/mlbigbook/wordcount/Count.scala
+++ src/main/scala/mlbigbook/wordcount/Count.scala
@@ -24,28 +24,3 @@ object Count {
 
   @inline private def add1(m: Map[String, Long], word: String) = AddMap.Whole.mark(m, word, 1)
 }
-
-object WordcountDriver {
-
-  def main(args: Array[String]) = {
-    val corpus = if (args.size > 1) {
-      args.slice(1, args.size)
-        .map(fi => Source.fromFile(fi).getLines())
-        .map(lines => Data.Document(lines.map(l => Data.Sentence(l.split(" "))).toIndexedSeq))
-        .toSeq
-    } else {
-      Seq(
-        Data.Document(
-          Source.fromInputStream(System.in)
-            .getLines()
-            .map(line => Data.Sentence(line.split(" ").toIndexedSeq))
-            .toIndexedSeq
-        ))
-    }
-
-    val sortedWordCount = Count.wordcountCorpus(corpus).toSeq.sortBy(x => -x._2)
-    for ((word, count) <- sortedWordCount) {
-      println(s"$word : $count")
-    }
-  }
-}
