0b426bdc406aa77ccb29ddf11f6e72dc2b713497
Malcolm Greaves <greaves.malcolm@gmail.com>
2014-11-09 21:28:26 -0800

    documented word count

--- src/main/scala/mlbigbook/wordcount/Count.scala
+++ src/main/scala/mlbigbook/wordcount/Count.scala
@@ -3,24 +3,38 @@ package mlbigbook.wordcount
 import scala.collection.Map
 import scala.io.Source
 
+/**
+ * The Count object contains methods for computing the word count on a corpus, document,
+ * and sentence level.
+ */
 object Count {
 
+  /**
+   * Compute the word counts across an entire corpus.
+   */
   def wordcountCorpus(documents: Data.Corpus): Data.WordCount = {
     documents
       .map(wordcountDocument)
       .aggregate(AddMap.Whole.empty)(AddMap.Whole.combine, AddMap.Whole.combine)
   }
 
+  /**
+   * Compute the word count on a document.
+   */
   def wordcountDocument(d: Data.Document): Data.WordCount = {
     d.sentences
       .map(wordcountSentence)
       .aggregate(AddMap.Whole.empty)(AddMap.Whole.combine, AddMap.Whole.combine)
   }
 
+  /**
+   * Compute the word count on a single sentence.
+   */
   def wordcountSentence(s: Data.Sentence): Data.WordCount = {
     s.words
       .aggregate(AddMap.Whole.empty)(add1, AddMap.Whole.combine)
   }
 
+  // Increment the count of a word in a mapping by 1.
   @inline private def add1(m: Map[String, Long], word: String) = AddMap.Whole.mark(m, word, 1)
 }
