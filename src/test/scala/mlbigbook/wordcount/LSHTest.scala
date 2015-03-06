5d49713b939fc009a401eb3e9e4b50f217248f3e
Malcolm Greaves <malcolm@alpinenow.com>
2015-03-06 13:25:25 -0800

    Conflicts: 	.gitignore 	src/main/scala/mlbigbook/wordcount/Count.scala 	src/main/scala/mlbigbook/wordcount/Data.scala 	src/main/scala/mlbigbook/wordcount/TFIDF.scala 	src/main/scala/mlbigbook/wordcount/Vector.scala 	src/test/scala/mlbigbook/wordcount/WordcountTest.scala

---
+++ src/test/scala/mlbigbook/wordcount/LSHTest.scala
@@ -0,0 +1,21 @@
+package mlbigbook.wordcount
+
+import scala.collection.Map
+
+import org.scalatest.{ BeforeAndAfterAll, Suite, FunSuite }
+
+import org.apache.spark.SparkContext
+import org.apache.log4j.{ Level, Logger }
+
+class LSHTest extends FunSuite {
+  import WordcountTest._
+  import LSHTest._
+
+  ignore("[seq] ranking using locally sensitive hashing") {
+
+  }
+}
+
+object LSHTest {
+
+}
