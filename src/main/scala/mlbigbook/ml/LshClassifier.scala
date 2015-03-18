5fec6d829319e90e4995c5ad578e17146e795954
Malcolm Greaves <greaves.malcolm@gmail.com>
2015-03-17 22:45:12 -0700

    finished LshClassifier

--- src/main/scala/mlbigbook/ml/LshClassifier.scala
+++ src/main/scala/mlbigbook/ml/LshClassifier.scala
@@ -1,89 +1,91 @@
 package mlbigbook.ml
 
-//import mlbigbook.data
-//import mlbigbook.data.{VectorizerMaker, DistDataContext, DistData, Data}
-//import mlbigbook.wordcount._
-//import mlbigbook.lsh.LSH
-//import scala.util.Random
-//
-//object LshClassifier {                                                                            h
-//
-//  type Type = Data.Document => Labeled
-//
-//  import Labeled._
-//
-//  type LabeledDocVector = (String, data.Vector)
-//
-//  def createHashTablesForCorpus(
-//    bandSize: Int,
-//    lshFuncs: Seq[LSH.Type],
-//    vectorizedLabeledData: DistData[(String, data.Vector)])(
-//      implicit ddContext: DistDataContext): Seq[DistData[LabeledDocVector]] = {
-//
-//    vectorizedLabeledData
-//      .map({
-//        case (label, vector) => ((label, vector), lshFuncs.map(h => h(vector)).toSet)
-//      })
-//      .flatMap({
-//        case (labeledVector, hashIndices) => hashIndices.map(hIndex => (hIndex, labeledVector))
-//      })
-//      .groupBy(_._1)
-//      .map({
-//        case (hIndex, iterable) => (hIndex, iterable.map(_._2))
-//      })
-//      .toSeq
-//      .map({
-//        case (hIndex, labeledVectorItr) => (hIndex, ddContext.from(labeledVectorItr))
-//      })
-//      .sortBy(_._1)
-//      .map(_._2)
-//  }
-//
-//  def apply(
-//    nLSHFuncs: Int,
-//    bandSize: Int)(
-//      dist: Distance,
-//      kNeighborhoodSize: Int)(
-//        mkVec: VectorizerMaker[Data.Document],
-//        labeledCorpus: LabeledCorpus)(implicit ddContext: DistDataContext): Type = {
-//
-//    val vectorizer = mkVec(labeledCorpus.corpus.map(_.example))
-//    val vectorizedLabeledData = labeledCorpus.corpus.map(d => (d.label, vectorizer(d.example)))
-//
-//    val lshFuncs = {
-//      val vectorspaceSize = vectorizedLabeledData.take(1).toSeq(0)._2.cardinality
-//      implicit val rand = new Random()
-//      LSH.create(nLSHFuncs, vectorspaceSize, bandSize)
-//    }
-//
-//    val hashTables = createHashTablesForCorpus(bandSize, lshFuncs, vectorizedLabeledData)
-//
-//    val perTableRankers = hashTables.map(ht =>
-//      (vecInputDoc: data.Vector) =>
-//        Ranker.takeTopK(
-//          kNeighborhoodSize,
-//          ht.map({ case (doc, vec) => (dist(vec, vecInputDoc), doc) })
-//        )
-//    )
-//
-//    (inputDoc: Data.Document) => {
-//
-//      val vecInputDoc = vectorizer(inputDoc)
-//
-//      val hashIndicies = lshFuncs.foldLeft(Set.empty[Int])(
-//        (hIndexSet, h) => hIndexSet + h(vecInputDoc)
-//      )
-//
-//      val documentsFromAllBins = hashIndicies.flatMap(hIndex => perTableRankers(hIndex)(vecInputDoc))
-//
-//      val topDocumentsFromBins = Rank.takeTopK(
-//        kNeighborhoodSize,
-//        documentsFromAllBins
-//      )
-//
-//      NearestNeighbors.takeLargest(
-//        NearestNeighbors.countNeighborhoodVotes(topDocumentsFromBins.map(_._2)).toIndexedSeq
-//      )
-//    }
-//  }
-//}
+import mlbigbook.data._
+import mlbigbook.lsh.LSH
+import scala.reflect.ClassTag
+import scala.util.Random
+
+object LshClassifier {
+
+  import Classifier._
+  import Labeled.str2labeled
+
+  def apply[T: ClassTag](
+    nLSHFuncs: Int,
+    bandSize: Int)(
+      dist: Distance,
+      kNeighborhoodSize: Int,
+      mkVec: VectorizerMaker[T],
+      labeledCorpus: DistData[LabeledData[T]])(
+        implicit ddContext: DistDataContext): Classifier[T] = {
+
+    val vectorizer = mkVec(labeledCorpus.map(_.example))
+    val vectorizedLabeledData = labeledCorpus.map(d => (d, vectorizer(d.example)))
+
+    val lshFuncs = {
+      val vectorspaceSize = vectorizedLabeledData.take(1).toSeq.head._2.cardinality
+      implicit val rand = new Random()
+      LSH(nLSHFuncs, vectorspaceSize, bandSize)
+    }
+
+    val hashTables = createHashTablesForCorpus(bandSize, lshFuncs, vectorizedLabeledData)
+
+    val perTableRankers = hashTables.map(ht =>
+      (vecInput: Vector) =>
+        Ranker.takeTopK[LabeledData[T], Double](
+          kNeighborhoodSize,
+          ht.map({
+            case (labeledData, vec) => (dist(vec, vecInput), labeledData)
+          })
+        )
+    ).toIndexedSeq
+
+    (input: T) => {
+
+      val vecInput = vectorizer(input)
+
+      val hashIndicies = lshFuncs.map(lsh => lsh(vecInput)).toSet
+
+      val examplesFromAllBins = hashIndicies.flatMap(hIndex => perTableRankers(hIndex)(vecInput))
+
+      val topLabelsFromAllBins = Ranker.takeTopK(
+        kNeighborhoodSize,
+        examplesFromAllBins
+      ).map(_._2.label)
+
+      str2labeled(
+        KnnClassifier.takeLargest(
+          KnnClassifier.countVotes(topLabelsFromAllBins)
+        )
+      )
+    }
+  }
+
+  def createHashTablesForCorpus[T](
+    bandSize: Int,
+    lshFuncs: Seq[LSH],
+    vectorizedLabeledData: DistData[(T, Vector)])(
+      implicit ddContext: DistDataContext): Seq[DistData[(T, Vector)]] =
+
+    vectorizedLabeledData
+      .map({
+        case (data, vector) =>
+          ((data, vector), lshFuncs.map(h => h(vector)).toSet)
+      })
+      .flatMap({
+        case (dataAndVector, hashIndices) =>
+          hashIndices.map(hIndex => (hIndex, dataAndVector))
+      })
+      .groupBy(_._1)
+      .map({
+        case (hIndex, iterable) =>
+          (hIndex, iterable.map(_._2))
+      })
+      .toSeq
+      .map({
+        case (hIndex, dataAndVectorItr) =>
+          (hIndex, ddContext.from(dataAndVectorItr))
+      })
+      .sortBy(_._1)
+      .map(_._2)
+}
