eba43fd6ddbece166e8640eac521c068a28b25c0
Malcolm Greaves <greaves.malcolm@gmail.com>
2015-03-20 16:32:50 -0700

    small - format change, del unused import

--- src/main/scala/mlbigbook/ml/LshClassifier.scala
+++ src/main/scala/mlbigbook/ml/LshClassifier.scala
@@ -7,13 +7,9 @@ import scala.reflect.ClassTag
 
 object LshClassifier {
 
-  import KnnClassifier._
-
   def apply[T: ClassTag](nLshFuncs: Int, nBins: Int)(n: NearNeighIn)(v: VectorDataIn[LabeledData[T]])(
     implicit ddContext: DistDataContext, rand: Random): Classifier[T] =
 
-    KnnClassifier(
-      LshRanker(nLshFuncs, nBins)(n)(v)(ddContext, rand)
-    )
+    KnnClassifier(LshRanker(nLshFuncs, nBins)(n)(v)(ddContext, rand))
 
 }
