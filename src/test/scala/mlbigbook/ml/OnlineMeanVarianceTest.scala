3818bd641cef6d6d2a8f811bf20347ad33310842
Malcolm Greaves <greaves.malcolm@gmail.com>
2015-08-09 09:54:08 -0700

    FAILING OnlineMeanVarianceTest

--- src/test/scala/mlbigbook/ml/OnlineMeanVarianceTest.scala
+++ src/test/scala/mlbigbook/ml/OnlineMeanVarianceTest.scala
@@ -37,6 +37,8 @@ class OnlineMeanVarianceTest extends FunSpec with Matchers {
 //    }
 //  }
 
+  import mlbigbook.util.NumericConversion.Implicits._
+
 
   describe("Welford's online variance calculation using generalized vectors"){
 
@@ -45,18 +47,30 @@ class OnlineMeanVarianceTest extends FunSpec with Matchers {
       import mlbigbook.data.Data
       import Data._
 
-      val data: Data[DenseVector[Double]] = Seq(DenseVector(12.0), DenseVector(-1.0), DenseVector(55.0))
+//      val data: Data[DenseVector[Double]] = Seq(DenseVector(12.0), DenseVector(-1.0), DenseVector(55.0))
       // mean:     20
       // variance: 20
+
+
+      lazy val rand = new Random(42L)
+      lazy val numEl = 1e6.toInt
+      lazy val data: Data[DenseVector[Double]] = Seq.fill(numEl)(DenseVector(rand.nextDouble() + 1e9))
+      lazy val expected = 0.083382083
+      lazy val tolerance = 1e-6
+
+
 
       import VectorOpsT._
 
       OnlineMeanVariance(data) match {
         case s @ OnlineMeanVariance.Stats(count, mean, variance) =>
           println(s"STATS: $s")
-          assert(count == 3l)
-          assert(mean(0) == 2.0)
-          assert(variance(0) == 2.0)
+          assert(count == numEl)
+          variance(0) should be(expected +- tolerance)
+//          assert(mean(0))
+//          assert(count == 3l)
+//          assert(mean(0) == 2.0)
+//          assert(variance(0) == 2.0)
       }
 
     }
